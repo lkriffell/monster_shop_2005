d6e24fbc9f85111af524b04fb45778f22ee5e9be
Logan Riffell <lkriffell@gmail.com>
2020-09-10 20:36:57 -0600

    Add code associated with authorization

---
+++ app/controllers/merchant/dashboard_controller.rb
@@ -0,0 +1,11 @@
+class Merchant::DashboardController < Admin::BaseController
+  before_action :require_merchant
+
+  def index
+  end
+
+  private
+    def require_merchant
+      render file: "/public/404" unless current_merchant?
+    end
+end
