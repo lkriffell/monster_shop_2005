1882720710debf0a489b5f6b9917a5efc6a22207
Angela Guardia <angelaguardia20@gmail.com>
2020-09-10 19:15:01 -0700

    Remove unnecessary validations for password in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,7 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates_presence_of :address, :city, :state, :zip, :password, :name
-
-
-
-  validates :password, confirmation: true
-  validates :password, presence: true
-  validates :password_confirmation, confirmation: true
-  validates :password_confirmation, presence: true
+  validates :password, confirmation: true, presence: true
 
   has_secure_password
 end
