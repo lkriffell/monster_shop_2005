23be313f8a49ccd0cb917ffea220539121897b0b
Angela Guardia <angelaguardia20@gmail.com>
2020-09-10 20:18:18 -0700

    Update validations for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
-  validates_presence_of :address, :city, :state, :zip, :password, :name
   validates :password, confirmation: true, presence: true
+  validates_presence_of :address, :city, :state, :zip, :password, :name
 
   has_secure_password
 end
