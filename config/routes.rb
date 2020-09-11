55379f86598befc61fb9f093a1629e911c56ea2f
Angela Guardia <angelaguardia20@gmail.com>
2020-09-11 12:41:34 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get '/profile', to: 'users#show'
 
   get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
 
   get "/merchants", to: "merchants#index"
   get "/merchants/new", to: "merchants#new"
