94a69338bc99d0a174b954b8dbbecfc98cc431ae
Arique <arique.aguilar@gmail.com>
2020-09-11 20:40:06 -0500

    Adds dashboard controller for admin

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,6 +1,5 @@
 class Merchant::DashboardController < ApplicationController
   def index
-    merchant = User.find(session[:user_id])
-    flash[:success] = "Hello, #{merchant.name}. You are now logged in."
+
   end
 end
