ba764d2fbef42c72088e6d1619744450850709cd
Kiera Allen <kieraaallen@gmail.com>
2020-09-11 20:54:26 -0600

    Create redirect helper method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,16 +7,20 @@ class SessionsController < ApplicationController
     if user.authenticate(session_params[:password])
       session[:user_id] = user.id
       flash[:success] = "Hello, #{user.name}. You are now logged in."
-      if user.merchant?
-        redirect_to '/merchant/dashboard'
-      elsif user.admin?
-        redirect_to '/admin/dashboard'
-      else
-        redirect_to '/profile'
-      end
+      redirect_user(user)
     else
       flash[:error] = "Sorry, your credentials are incorrect."
       render :new
+    end
+  end
+
+  def redirect_user(user)
+    if user.merchant?
+      redirect_to '/merchant/dashboard'
+    elsif user.admin?
+      redirect_to '/admin/dashboard'
+    else
+      redirect_to '/profile'
     end
   end
 
