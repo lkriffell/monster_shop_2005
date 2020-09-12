f39b97bc05d73cc84f77c1ca2b473357e85a86cb
Angela Guardia <angelaguardia20@gmail.com>
2020-09-11 18:01:29 -0700

    Create a test for logging is as a regular and merchant user

--- spec/features/sessions/new_spec.rb
+++ spec/features/sessions/new_spec.rb
@@ -15,4 +15,21 @@ describe "Logging In as" do
 
     expect(page).to have_content("Hello, #{user.name}. You are now logged in.")
   end
+
+  it 'merchant user in with valid credentials' do
+    merchant = User.create!(name: "bob", password: '12345', address: "street", city: "Denver", state: "CO", zip:"12345", email: "someone@gmail.com", role: 1)
+
+    visit "/login"
+
+    fill_in :email, with: merchant.email
+    fill_in :password, with: merchant.password
+
+    click_button "Log In"
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(merchant)
+
+    expect(current_path).to eq("/merchant/dashboard")
+
+    expect(page).to have_content("Hello, #{merchant.name}. You are now logged in.")
+  end
 end
