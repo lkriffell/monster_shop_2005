f67b7df072d88e89df11da8c6a6dafce0a357b7e
Logan Riffell <lkriffell@gmail.com>
2020-09-13 11:20:56 -0600

    Update to use current_user

--- app/controllers/merchant/dashboard_controller.rb
+++ app/controllers/merchant/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class Merchant::DashboardController < Merchant::BaseController
   def index
-      user = current_user
-      @merchant = Merchant.find(user.merchant_id)
+      current_user
+      @merchant = Merchant.find(current_user.merchant_id)
       @orders = @merchant.orders.joins(:item_orders)
   end
 end
