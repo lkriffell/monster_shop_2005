d7789a60baf14e55af3e8d9855c765bd15fade8a
Arique <arique.aguilar@gmail.com>
2020-09-12 22:04:47 -0500

    Adds test for making sure items added to cart don't surpass the inventory limit.

--- spec/features/cart/add_item_spec.rb
+++ spec/features/cart/add_item_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Cart show' do
         @mike = Merchant.create(name: "Mike's Print Shop", address: '123 Paper Rd.', city: 'Denver', state: 'CO', zip: 80203)
         @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
 
-        @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+        @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 3)
         @paper = @mike.items.create(name: "Lined Paper", description: "Great for writing on!", price: 20, image: "https://cdn.vertex42.com/WordTemplates/images/printable-lined-paper-wide-ruled.png", inventory: 25)
         @pencil = @mike.items.create(name: "Yellow Pencil", description: "You can write on paper with it!", price: 2, image: "https://images-na.ssl-images-amazon.com/images/I/31BlVr01izL._SX425_.jpg", inventory: 100)
         visit "/items/#{@tire.id}"
@@ -42,6 +42,28 @@ RSpec.describe 'Cart show' do
         expect(page).to have_css("#cart-item-#{@paper.id}")
 
         expect(page).to have_content("Total: $222.00")
+      end
+
+      it "can NOT add more items then there are inventory" do
+        visit "/cart"
+
+        @items_in_cart.each do |item|
+          within "#cart-item-#{item.id}" do
+            expect(page).to have_link("Add More")
+          end
+        end
+      end
+
+      it 'I can add individual items from my cart' do
+        visit "/cart"
+
+        within "#cart-item-#{@tire.id}" do
+          click_on "Add More"
+          click_on "Add More"
+          click_on "Add More"
+        end
+        expect(current_path).to eq("/cart")
+        expect(page).to have_content("No more in stock")
       end
     end
   end
