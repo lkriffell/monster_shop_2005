d30ef6d0d8650805a5498d2da5e64ea28291bb8d
Arique <arique.aguilar@gmail.com>
2020-09-13 20:09:29 -0500

    Adds update method to cart_controller

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -23,10 +23,29 @@ class CartController < ApplicationController
     session[:cart].delete(params[:item_id])
     redirect_to '/cart'
   end
-
-  def add_more_item
+#this is the beginning of a refactor of the update method
+  # def update
+  #   require "pry"; binding.pry
+  #   case params[:add]
+  #   when true
+  #     if item.inventory_has_reached_limit?(cart)
+  #       flash[:error] = "You've reached the end of this merchant's inventory"
+  #       redirect_to '/cart'
+  #     else
+  #       cart.add_item(item.id.to_s)
+  #       redirect_to '/cart'
+  #     end
+  #   else
+  #
+  #   end
+  #
+  #   redirect_to "/cart"
+  #
+  #
+  # end
+  def update
     item = Item.find(params[:item_id])
-
+    if params[:add] == "true"
       if item.inventory_has_reached_limit?(cart, item)
         flash[:error] = "You've reached the end of this merchant's inventory"
         redirect_to '/cart'
@@ -34,6 +53,16 @@ class CartController < ApplicationController
         cart.add_item(item.id.to_s)
         redirect_to '/cart'
       end
+
+    else params[:add] == "false"
+      if cart.contents[item.id.to_s] == 1
+        session[:cart].delete(params[:item_id])
+        redirect_to '/cart'
+      else
+        cart.remove_item(item.id.to_s)
+        redirect_to "/cart"
+      end
+    end
   end
 
 end
