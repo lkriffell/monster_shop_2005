754099435607d67cfbdeb1175a6d30eb0b841d6b
Logan Riffell <lkriffell@gmail.com>
2020-09-18 08:54:41 -0600

    Add test for add_back_to_inventory

--- spec/models/item_orders_spec.rb
+++ spec/models/item_orders_spec.rb
@@ -24,8 +24,32 @@ describe ItemOrder, type: :model do
       expect(item_order_1.subtotal).to eq(200)
     end
 
-    
- 
+    it "add_back_to_inventory" do
+
+      @regular_user = User.create!(name: "barb", password: '12345', address: "street", city: "Denver", state: "CO", zip:"90210", email: "someone@gmail.com", role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@regular_user)
+
+      @bike_shop = Merchant.create!(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: "80203")
+
+      @merchant_user = @bike_shop.users.create!(name: "barb", password: '12345', address: "street", city: "Denver", state: "CO", zip:"90210", email: "somefour@gmail.com", role: 1)
+
+      @tire = @bike_shop.items.create!(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+
+      @order = Order.create!(name: "name", address: "address", city: "city", state: "state", zip: "80210", user_id: @regular_user.id, status: "pending")
+
+
+      item_order = ItemOrder.create!(order_id: @order.id, price: 1.0, item_id: @tire.id, quantity: 4, status: 1)
+
+      item_order.add_back_to_inventory
+
+      @tire.reload
+
+      expect(@tire.inventory).to eq(16)
+
+    end
+
+
   end
 
 end
