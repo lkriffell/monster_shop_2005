fda44489b9c70d1e318080ad34f36a3103231407
Logan Riffell <lkriffell@gmail.com>
2020-09-29 11:00:06 -0600

    Refactor routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   patch '/profile', to: 'users#update'
   patch '/profile/password', to: 'users#update_password'
 
-
+  # resources :login, only: [:new, :create]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
@@ -32,23 +32,16 @@ Rails.application.routes.draw do
     get '/items', to: "items#index"
     get '/orders/:id', to: "orders#show"
     get "/item_orders/:id/fulfill", to: "item_orders#update"
-    get '/items/:merchant_id', to: "items#index"
-    patch '/items/:item_id', to: "items#update"
-    delete '/items/:item_id', to: "items#destroy"
+    # namespace :items do
+      get '/items/:merchant_id', to: "items#index"
+      patch '/items/:item_id', to: "items#update"
+      delete '/items/:item_id', to: "items#destroy"
+    # end
   end
 
-  get "/merchants", to: "merchants#index"
-  get "/merchants/new", to: "merchants#new"
-  get "/merchants/:id", to: "merchants#show"
-  post "/merchants", to: "merchants#create"
-  get "/merchants/:id/edit", to: "merchants#edit"
-  patch "/merchants/:id", to: "merchants#update"
-  delete "/merchants/:id", to: "merchants#destroy"
+  resources :merchants
+  resources :items, only: [:index, :show, :edit, :update]
 
-  get "/items", to: "items#index"
-  get "/items/:id", to: "items#show"
-  get "/items/:id/edit", to: "items#edit"
-  patch "/items/:id", to: "items#update"
   get "/merchants/:merchant_id/items", to: "items#index"
   get "/merchants/:merchant_id/items/new", to: "items#new"
   post "/merchants/:merchant_id/items", to: "items#create"
@@ -57,13 +50,9 @@ Rails.application.routes.draw do
   get "/items/:item_id/reviews/new", to: "reviews#new"
   post "/items/:item_id/reviews", to: "reviews#create"
 
-  get "/reviews/:id/edit", to: "reviews#edit"
-  patch "/reviews/:id", to: "reviews#update"
-  delete "/reviews/:id", to: "reviews#destroy"
+  resources :reviews, only: [:edit, :update, :destroy]
 
   patch "cart/:item_id", to: "cart#update"
-  # patch "/:item_id", action: :update, controller: "items"
-  #above route is a  name space, namespace: cart do
   post "/cart/:item_id", to: "cart#add_item"
   get "/cart", to: "cart#show"
   delete "/cart", to: "cart#empty"
@@ -72,7 +61,6 @@ Rails.application.routes.draw do
   get "/profile/orders", to: "orders#index"
   patch '/profile/orders/:order_id/edit', to: 'orders#update'
   get '/profile/orders/:order_id', to: 'orders#show'
-  get "/orders/new", to: "orders#new"
-  post "/orders", to: "orders#create"
-  get "/orders/:id", to: "orders#show"
+
+  resources :orders, only: [:new, :create, :show]
 end
